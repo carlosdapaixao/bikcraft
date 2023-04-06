49b54954a632ee24d98c381590f468a0b22313e3
Carlos da Paixão <50736789+carlosdapaixao@users.noreply.github.com>
2023-04-06 14:32:24 +0100

    Alteração no js, ativação do produto com base nos parametros da url

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Link do nenú ativo
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -9,3 +10,18 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+//Ativar itens do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }  
+  console.log(elemento);
+}
+
+parametros.forEach(ativarProduto);
+
+console.info(parametros);
