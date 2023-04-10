3d965eac2584e33337988f966f67e55af15e6510
Carlos da Paixão <50736789+carlosdapaixao@users.noreply.github.com>
2023-04-10 11:57:19 +0100

    adicionado script de perguntas frequentes com ligeiras alterações no css

--- js/script.js
+++ js/script.js
@@ -18,10 +18,26 @@ function ativarProduto(parametro) {
   const elemento = document.getElementById(parametro);
   if (elemento) {
     elemento.checked = true;
-  }  
-  console.log(elemento);
+  }
 }
 
 parametros.forEach(ativarProduto);
 
-console.info(parametros);
+//Perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+  
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventoPerguntas(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventoPerguntas);
